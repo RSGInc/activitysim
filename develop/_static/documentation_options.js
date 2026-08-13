a10dadfaf0bb57c66683ef7097a262e8c25ac32a
dhensle <dhensle@users.noreply.github.com>
2026-08-13 17:55:03 +0000

    deploy: 2b73d5a8b333462a1b7a0108e776551e487d6f29

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.5.2.dev20+gaa1a3ff1c',
+    VERSION: '1.5.2.dev28+g2b73d5a8b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
